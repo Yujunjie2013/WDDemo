d6e7ac1b6c51be4dcc70decd8d2ac96a6e51e91c
Yune <819828037@qq.com>
2018-08-26 21:59:23 +0800

    小程序代码

---
+++ pages/index/index.js
@@ -0,0 +1,35 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    list: [{
+        id: "form",
+        name: "表单",
+        open: false
+      },
+      {
+        id: "widget",
+        name: "基础组件",
+        open: false
+      },
+      {
+        id: "feedback",
+        name: "操作反馈",
+        open: false
+      },
+      {
+        id: "nav",
+        name: "导航相关",
+        open: false
+      },
+      {
+        id: "search",
+        name: "搜索",
+        open: false
+      }
+    ]
+  },
+
+})
